9f14f1734952094916459fb67104b653b3ce2b9e
Igor de Almeida <igoor1205@gmail.com>
2025-07-25 14:13:44 -0300

    refactor: make some adjustments

--- apps/ecommerce_products/src/ProductList.tsx
+++ apps/ecommerce_products/src/ProductList.tsx
@@ -2,13 +2,11 @@ import { useCartEvent, Product, useCartStore } from "ecommerce_home/cartStore";
 
 import './index.css'
 
-
 const products: Product[] = [
   { id: "1", name: "Product 1", price: 10 },
   { id: "2", name: "Product 2", price: 20 },
   { id: "3", name: "Product 3", price: 30 },
 ];
-
 
 export default function ProductList() {
   const { dispatchAddItem } = useCartEvent();
